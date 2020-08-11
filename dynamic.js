dd0e22b3a4a9a0f8a6da78d6fa4937e23c06b474
Justin Hutchins <Justin.Hutchins@learninga-z.com>
2020-08-11 13:53:52 -0400

    Adding underline effect and improving scrollbar

--- dynamic.js
+++ dynamic.js
@@ -1,3 +1,18 @@
+;(function($, win) {
+    $.fn.inViewport = function(cb) {
+        return this.each(function(i,el) {
+            function visPx(){
+                var elH = $(el).outerHeight(),
+                    H = $(win).height(),
+                    r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+                return cb.call(el, Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H)));
+            }
+            visPx();
+            $(win).on("resize scroll", visPx);
+        });
+    };
+}(jQuery, window));
+
 $(document).ready(function() {
     $("a").on('click', function(event) {
         if (this.hash !== "") {
@@ -9,5 +24,9 @@ $(document).ready(function() {
                 window.location.hash = hash;
             });
         }
+    });
+
+    $(".header-line").inViewport(function(px) {
+        $(this).toggleClass("expand-from-center", !!px);
     });
 });
