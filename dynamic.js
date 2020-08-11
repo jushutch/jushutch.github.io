89e6d2ceadb1b31d945d885e1a335179d1a1ecc3
Justin Hutchins <Justin.Hutchins@learninga-z.com>
2020-08-11 14:16:35 -0400

    Trying different approach for underlining

--- dynamic.js
+++ dynamic.js
@@ -1,19 +1,46 @@
-;(function($, win) {
-    $.fn.inViewport = function(cb) {
-        return this.each(function(i,el) {
-            function visPx(){
-                var elH = $(el).outerHeight(),
-                    H = $(win).height(),
-                    r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
-                return cb.call(el, Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H)));
-            }
-            visPx();
-            $(win).on("resize scroll", visPx);
-        });
-    };
-}(jQuery, window));
+//;(function($, win) {
+//    $.fn.inViewport = function(cb) {
+//        return this.each(function(i,el) {
+//            function visPx(){
+//                var elH = $(el).outerHeight(),
+//                    H = $(win).height(),
+//                    r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+//                return cb.call(el, Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H)));
+//            }
+//            visPx();
+//            $(win).on("resize scroll", visPx);
+//        });
+//    };
+//}(jQuery, window));
 
 $(document).ready(function() {
+    var $animation_elements = $('.header-line');
+    var $window = $(window);
+
+    function check_if_in_view() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
+
+        $.each($animation_elements, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('expand-from-center');
+            } else {
+                $element.removeClass('expand-from-center');
+            }
+        });
+    }
+
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+
     $("a").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
@@ -26,7 +53,7 @@ $(document).ready(function() {
         }
     });
 
-    $(".header-line").inViewport(function(px) {
-        $(this).toggleClass("expand-from-center", !!px);
-    });
+    //$(".header-line").inViewport(function(px) {
+    //    $(this).toggleClass("expand-from-center", !!px);
+    //});
 });
