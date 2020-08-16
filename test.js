780fd2b5c14bbf48fdcabef73c0dca719eec6216
jushutch <jushutch@umich.edu>
2020-08-16 15:52:09 -0400

    Removing deprecated files

--- test.js
+++
@@ -1,7 +0,0 @@
-document.write("Hello World from JavaScript!<br>")
-names = ["Chester", "aRthuR", "OlivEr", "QUINN", "MAlcolm"];
-
-for (i = 0; i < names.length; ++i) {
-  names[i] = names[i].charAt(0).toUpperCase() + names[i].substr(1).toLowerCase();
-  document.write(names[i] + "<br>");
-}
